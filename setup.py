2b24da214ef76e60ebc36a559f1bc302975905f2
Jerry Ma <jerry.ma.nk@gmail.com>
2022-03-04 10:32:22 -0500

    update pypi package data

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import json
-import os
 from setuptools import setup
+from pathlib import Path
 
+HERE = Path(__file__).parent
 
-with open('package.json') as f:
+with open(HERE.joinpath('package.json')) as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+homepage = package['homepage']
+repo_url = package['homepage']
+bugs_url = package['bugs']['url']
+long_description = (HERE / "README.md").read_text()
+
 
 setup(
     name=package_name,
@@ -16,8 +23,18 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
-    install_requires=[],
-    classifiers = [
-        'Framework :: Dash',
-    ],    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=homepage,
+    project_urls={
+        "Bug Reports": bugs_url,
+        "Source": repo_url,
+    },
+    install_requires=["dash"],
+    classifiers=[
+        "Framework :: Dash",
+        'License :: OSI Approved :: BSD License',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
